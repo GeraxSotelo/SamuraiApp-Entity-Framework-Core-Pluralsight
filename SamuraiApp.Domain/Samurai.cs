af4fd32dafa4c3d2e3b547bb44b8c3be59971232
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-22 12:53:31 -0600

    Add Horse class & update Samurai class

--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -16,5 +16,7 @@ namespace SamuraiApp.Domain
         public List<Quote> Quotes { get; set; }
         public Clan Clan { get; set; }
         public List<SamuraiBattle> SamuraiBattles { get; set; }
+        public Horse Horse { get; set; }
+        //With the navigation property in Samurai and the foreign key in the Horse class, it's enough for EF Core to comprehend the relationship without any more information
     }
 }
