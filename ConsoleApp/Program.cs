a75a71a3c021a23111bf0a30b42909f32d7d2ff6
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-22 21:15:41 -0600

    Filtering queries

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
 using System.Linq;
@@ -7,16 +8,17 @@ namespace ConsoleApp
 {
     internal class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
+        private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
             //This is just a quick hack. DO NOT use this in real software
-            context.Database.EnsureCreated(); //This will cause EF Core to read the provider & connection string defined in the Context class, and then go look to see it the db exists
+            _context.Database.EnsureCreated(); //This will cause EF Core to read the provider & connection string defined in the Context class, and then go look to see it the db exists
             //This is just a quick hack. DO NOT use this in real software
             //GetSamurais("Before Add:");
             //AddSamurai();
             //GetSamurais("After Add:");
-            InsertMultipleSamurais();
+            //InsertMultipleSamurais();
+            QueryFilters();
             Console.WriteLine("Press any key...");
             Console.ReadKey();
         }
@@ -27,34 +29,43 @@ namespace ConsoleApp
             var samurai2 = new Samurai { Name = "Samurai-2" };
             var samurai3 = new Samurai { Name = "Samurai-3" };
             var samurai4 = new Samurai { Name = "Samurai-4" };
-            context.AddRange(samurai, samurai2, samurai3, samurai4);
-            context.SaveChanges();
+            _context.AddRange(samurai, samurai2, samurai3, samurai4);
+            _context.SaveChanges();
         }
 
         private static void InsertVariousTypes()
         {
             var samurai = new Samurai { Name = "Samurai-5" };
             var clan = new Clan { ClanName = "Imperial Clan" };
-            context.AddRange(samurai, clan);
-            context.SaveChanges();
+            _context.AddRange(samurai, clan);
+            _context.SaveChanges();
         }
 
         private static void AddSamurai()
         {
             var samurai = new Samurai { Name = "Sampson" };
-            context.Samurais.Add(samurai); //Add to context.Samurais DbSet, so it's an in-memory collection of samurais that the context keeps track of
-            context.SaveChanges();//Save the data that the context is tracking back to the db
+            _context.Samurais.Add(samurai); //Add to context.Samurais DbSet, so it's an in-memory collection of samurais that the context keeps track of
+            _context.SaveChanges();//Save the data that the context is tracking back to the db
         }
 
         private static void GetSamurais(string text)
         {
-            var samurais = context.Samurais.ToList();//LINQ query to retrieve all the samurais from the db as objects.
+            var samurais = _context.Samurais.ToList();//LINQ query to retrieve all the samurais from the db as objects.
             //ToList() LINQ method forces the query to execute and returns a list of the objects
             Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
             foreach (var samurai in samurais)
             {
                 Console.WriteLine(samurai.Name);
             }
+        }
+
+        private static void QueryFilters()
+        {
+            var name = "Sampson";
+            //var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
+
+            //% is a wildcard
+            var samurais = _context.Samurais.Where(s => EF.Functions.Like(s.Name, "J%")).ToList();
         }
     }
 }
