bcdc7d206fe6aa152a7fa83c98e9acb3f6237c33
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-29 14:42:46 -0600

    Fix names

--- SamuraiApp.Tests/InMemoryTests.cs
+++ SamuraiApp.Tests/InMemoryTests.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Diagnostics;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using Xunit;
 
-namespace VirtualFileSystem.Tests
+namespace SamuraiApp.Tests
 {
     public class InMemoryTests
     {
@@ -15,14 +13,14 @@ namespace VirtualFileSystem.Tests
             var builder = new DbContextOptionsBuilder();
             //UsInMemoryDatabase() needs a string to be passed in to give a name to that instance
             //It's how EF Core keeps track of different instances of the provider in memory.
-            builder.UseInMemoryDatabase("CanInsertDirectory");
+            builder.UseInMemoryDatabase("CanInsertSamurai");
 
             using (var context = new SamuraiContext(builder.Options))
             {
-                var dir = new Samurai();
+                var samurai = new Samurai();
 
                 //The InMemory provider is just lists in memory & even though it knows how to increment IDs, it does it as the entity is being introduced into the change tracker (which is different than what happens if you're waiting for a db to generate that ID
-                context.Samurais.Add(dir);
+                context.Samurais.Add(samurai);
 
                 //In this case, SaveChanges() doesn't really impact the ID beyond what calling Add has done.
                 //context.SaveChanges();
@@ -30,9 +28,9 @@ namespace VirtualFileSystem.Tests
                 //This Assertion only makes sense when testing with a real db
                 //Assert.NotEqual(0, dir.Id);
 
-                //Make sure the FileSystemContext knows what to do when asked to Add()
+                //Make sure the SamuraiContext knows what to do when asked to Add()
                 //Check the state of the entity is added.
-                Assert.Equal(EntityState.Added, context.Entry(dir).State);
+                Assert.Equal(EntityState.Added, context.Entry(samurai).State);
             }
         }
     }
